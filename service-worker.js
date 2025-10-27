383e757b90ff07ee0a0030bb0f59317befb85e7a
JeepScape <harrisonjmcdonough@gmail.com>
2025-10-27 11:34:58 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,30 @@
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('gym-plan-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.webmanifest',
-      './icon-192.png',
-      './icon-512.png'
-    ]))
+const CACHE = 'gym-plan-v3';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        if (event.request.method === 'GET' && resp.ok) {
+          caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+        }
+        return resp;
+      }).catch(() => cached);
+      return cached || network;
+    })
   );
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
